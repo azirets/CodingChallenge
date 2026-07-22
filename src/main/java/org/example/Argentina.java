c116d4b089834029a5697b37d5e6d4cc49f2858b
Aziret <azik927@gmail.com>
2026-07-22 07:04:54 +0300

    argentina bebebebeb

--- src/main/java/org/example/Argentina.java
+++ src/main/java/org/example/Argentina.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Argentina {
     public static void main(String[] args) {
-        System.out.println("argentina is not champions");
+        System.out.println("argentina is not champions bebebebebebee");
     }
 }
