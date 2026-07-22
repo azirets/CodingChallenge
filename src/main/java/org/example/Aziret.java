4e2b4e46b3df3164e8bd728754f950ede5068223
Aziret <azik927@gmail.com>
2026-07-22 07:07:10 +0300

    aziret changed

--- src/main/java/org/example/Aziret.java
+++ src/main/java/org/example/Aziret.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Aziret {
     public static void main(String[] args) {
-        System.out.println("Azirettt");
+        System.out.println("sdnkjsnksmdlfksnk");
     }
 }
