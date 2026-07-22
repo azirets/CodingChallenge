5e685ab63c0f3bf2f00c0474716bb7b95484de8e
Aziret <azik927@gmail.com>
2026-07-22 06:34:05 +0300

    Hello world in feature

--- src/main/java/org/example/conflict/ConflictClass.java
+++ src/main/java/org/example/conflict/ConflictClass.java
@@ -2,7 +2,7 @@ package org.example.conflict;
 
 public class ConflictClass {
     public static void main(String[] args) {
-        System.out.println("This is my conflict class!");
+        System.out.println("Hello World");
 
     }
 }
